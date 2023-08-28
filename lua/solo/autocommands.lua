5f9133e67d71b68efb37acd42a61ff7653345f3f
Kristofers Solo <kristians.cagulis@pm.me>
2023-08-28 21:47:35 +0300

    Removed `fugitive` autocmd's

--- lua/solo/autocommands.lua
+++ lua/solo/autocommands.lua
@@ -84,32 +84,6 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 	end,
 })
 
--- vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
--- vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
--- 	group = vim.api.nvim_create_augroup("Fugitive", {}),
--- 	pattern = "*",
--- 	callback = function()
--- 		if vim.bo.ft ~= "fugitive" then
--- 			return
--- 		end
---
--- 		local bufnr = vim.api.nvim_get_current_buf()
--- 		local opts = { buffer = bufnr, remap = false }
--- 		vim.keymap.set("n", "<leader>p", function()
--- 			vim.cmd.Git("push")
--- 		end, opts)
---
--- 		-- rebase always
--- 		vim.keymap.set("n", "<leader>P", function()
--- 			vim.cmd.Git({ "pull", "--rebase" })
--- 		end, opts)
---
--- 		-- NOTE: It allows me to easily set the branch I am pushing and any tracking
--- 		-- needed if i did not set the branch up correctly
--- 		vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
--- 	end,
--- })
-
 -- Remove trailing whitespaces on save
 -- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 -- 	group = vim.api.nvim_create_augroup("RemoveTrailingWhitespaces", { clear = true }),
