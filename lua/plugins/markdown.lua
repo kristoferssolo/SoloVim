b980cce75bf0aed0eb084ece6e54b7d905183928
Kristofers Solo <dev@kristofers.xyz>
2025-06-27 14:19:24 +0300

    Update 2025-06-27

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -10,12 +10,12 @@ return {
 				inline_surround_toggle_line = "gss", -- (string|boolean) line-wise toggle inline style
 				inline_surround_delete = "ds", -- (string|boolean) delete emphasis surrounding cursor
 				inline_surround_change = "cs", -- (string|boolean) change emphasis surrounding cursor
-				link_add = "gl", -- (string|boolean) add link
-				link_follow = "gx", -- (string|boolean) follow link
-				go_curr_heading = "]c", -- (string|boolean) set cursor to current section heading
+				link_add = "gl",         -- (string|boolean) add link
+				link_follow = "gx",      -- (string|boolean) follow link
+				go_curr_heading = "]c",  -- (string|boolean) set cursor to current section heading
 				go_parent_heading = "]p", -- (string|boolean) set cursor to parent section heading
-				go_next_heading = "]]", -- (string|boolean) set cursor to next section heading
-				go_prev_heading = "[[", -- (string|boolean) set cursor to previous section heading
+				go_next_heading = "]]",  -- (string|boolean) set cursor to next section heading
+				go_prev_heading = "[[",  -- (string|boolean) set cursor to previous section heading
 			},
 			inline_surround = {
 				-- For the emphasis, strong, strikethrough, and code fields:
@@ -66,7 +66,9 @@ return {
 	},
 	{
 		"OXY2DEV/markview.nvim",
-		-- enabled = false,
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+		},
 		ft = { "markdown", "Avante" },
 		lazy = false,
 		opts = {
