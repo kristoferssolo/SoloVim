3fed3097ebe64b1c57cd78baafb0ab488702561f
Kristofers Solo <dev@kristofers.xyz>
2026-09-04 00:24:44 +0300

    fix: resolve DotenvGet in dadbod config hook

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -18,8 +18,10 @@ return {
 		"DBUIAddConnection",
 		"DBUIFindBuffer",
 	},
-	init = function()
-		-- Your DBUI configuration
+	config = function()
 		vim.g.db_ui_use_nerd_fonts = 1
+		vim.g.dbs = {
+			dev = vim.fn.DotenvGet("DATABASE_URL"),
+		}
 	end,
 }
